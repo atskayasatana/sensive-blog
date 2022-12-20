ba47b96e783a961ee5c4f7fd1e845df9cd8054dd
atskayasatana <88509622+atskayasatana@users.noreply.github.com>
2022-12-20 22:24:09 +0300

    c#3: fixed pep8 errors

--- blog/admin.py
+++ blog/admin.py
@@ -1,17 +1,18 @@
 from django.contrib import admin
 from blog.models import Post, Tag, Comment
 
+
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
     list_display = ('author', 'post', 'text',)
     raw_id_fields = ('post', 'author')
 
+
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     pass
 
+
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
     pass
-
-
